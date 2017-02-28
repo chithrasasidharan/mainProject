1f67e296fa6ddd53a2bc69c256cbd43b27ac1398
chithra <chithratheertham@gmail.com>
2017-02-28 17:26:18 +0530

    changes

--- circle.cpp
+++ circle.cpp
@@ -8,6 +8,10 @@
 #include <opencv2/video.hpp>
 #include "opencv2/video/background_segm.hpp"
 
+#define loopCountSec 40
+#define avgLoopCount 50	
+#define radius 100/32
+
 using namespace cv;
 using namespace std;
 Ptr<BackgroundSubtractor>pMOG2;
@@ -21,42 +25,33 @@ void MyFilledCircle( Mat img, Point center )
 
  circle( img,
          center,
-         100/32.0,
+         radius,
          Scalar( 0, 0, 255 ),
          thickness,
          lineType );
 }
 void addPoints(){
-	Point pt1=Point(200,200);
-	Point pt2=Point(210,350);
-	Point pt3=Point(240,160);
-	Point pt4=Point(290,135);
-	Point pt5=Point(340,175);
-	Point pt6=Point(240,370);
-	Point pt7=Point(280,385);
-	Point pt8=Point(320,365);
-	Point pt9=Point(200,275);
-	Point pt10=Point(350,275);
-	pointList.push_back(pt1);
-	pointList.push_back(pt2);
-	pointList.push_back(pt3);
-	pointList.push_back(pt4);
-	pointList.push_back(pt5);
-	pointList.push_back(pt6);
-	pointList.push_back(pt7);
-	pointList.push_back(pt8);
-	pointList.push_back(pt9);
-	pointList.push_back(pt10);
+	pointList.push_back(Point(200,200));
+	pointList.push_back(Point(210,350));
+	pointList.push_back(Point(240,160));
+	pointList.push_back(Point(290,135));
+	pointList.push_back(Point(340,175));
+	pointList.push_back(Point(240,370));
+	pointList.push_back(Point(280,385));
+	pointList.push_back(Point(320,365));
+	pointList.push_back(Point(200,275));
+	pointList.push_back(Point(350,275));
 
 }
 
 int main(){
 	VideoCapture cap(0);
+	int loopCtr=0;
 	pMOG2 = new BackgroundSubtractorMOG2();
 	if(!cap.isOpened())
 		return -1;
 	while(1){
-		
+		loopCtr++;
 		Mat frame;
 		cap>>frame;
 		addPoints();
