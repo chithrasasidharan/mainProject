25a5eb48fddeb059bd1bf6ceaee79a95274ca030
chithra <chithratheertham@gmail.com>
2017-03-02 22:11:24 +0530

    Averaging colour

--- circle.cpp
+++ circle.cpp
@@ -41,9 +41,32 @@ void addPoints(){
 	pointList.push_back(Point(320,365));
 	pointList.push_back(Point(200,275));
 	pointList.push_back(Point(350,275));
-
 }
 
+void getAvgColor(Mat img){
+    Point pts[1][10];
+    pts[0][0] = Point(200,200);
+	pts[0][1] = Point(210,350);
+	pts[0][2] = Point(240,160);
+	pts[0][3] = Point(290,135);
+	pts[0][4] = Point(340,175);
+	pts[0][5] = Point(240,370);
+	pts[0][6] = Point(280,385);
+	pts[0][7] = Point(320,365);
+	pts[0][8] = Point(200,275);
+	pts[0][9] = Point(350,275);
+    const Point* points[1] = {pts[0]};
+    int npoints = 10;
+
+    // Create the mask with the polygon
+    Mat mask(img.rows, img.cols, uchar(0));
+    fillPoly(mask, points, &npoints, 1, Scalar(255));
+
+    // Compute the mean with the computed mask
+    Scalar average = mean(img, mask);
+
+    std::cout << average << std::endl;
+}
 int main(){
 	VideoCapture cap(0);
 	int loopCtr=0;
@@ -55,6 +78,9 @@ int main(){
 		Mat frame;
 		cap>>frame;
 		addPoints();
+
+		cvtColor(frame, frame, CV_BGR2HSV);
+
 		if(loopCtr<=loopCountSec){
 			for(int i=0; i<pointList.size(); i++)
 				circle(frame,pointList[i],radius,Scalar(255,0,0),-1,8);
@@ -63,6 +89,7 @@ int main(){
 			for(int i=0; i<pointList.size(); i++)
 				circle(frame,pointList[i],radius,Scalar(255,255,255),-1,8);
 		}
+		getAvgColor(frame);
 		imshow("image",frame);
 		if(waitKey(30)>=0)
 			break;
