615d7d23d00ff8a2cfbdd0f3984c7b73db3d3bbe
moonblade <moonblade168@gmail.com>
2017-04-19 16:03:04 +0530

    hand partly done

--- region.cpp
+++ region.cpp
@@ -27,3 +27,34 @@ Region::Region(Point tlc, Mat src){
 void Region::draw(Mat src){
     rectangle(src, tl, br, borderColor, borderThickness);
 }
+
+int Region::getMedian(vector<int> values)
+{
+  int median;
+  size_t size = values.size();
+  sort(values.begin(), values.end());
+  if (size  % 2 == 0)  {
+      median = values[size / 2 - 1] ;
+  } else{
+      median = values[size / 2];
+  }
+  return median;
+}
+
+void Region::getAverage(Mat src, int averageColors[3]){
+    Mat temp;
+    region.copyTo(temp);
+    vector<int> hm;
+    vector<int> sm;
+    vector<int> lm;
+	for(int i=borderThickness; i<temp.rows-borderThickness; i++){
+    	for(int j=borderThickness; j<temp.cols-borderThickness; j++){
+    		hm.push_back(temp.data[temp.channels()*(temp.cols*i + j) + 0]) ;
+        	sm.push_back(temp.data[temp.channels()*(temp.cols*i + j) + 1]) ;
+        	lm.push_back(temp.data[temp.channels()*(temp.cols*i + j) + 2]) ;
+   		}
+	}
+    averageColors[0]=getMedian(hm);
+	averageColors[1]=getMedian(sm);
+	averageColors[2]=getMedian(lm);
+}
