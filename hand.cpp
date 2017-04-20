f906309f822d73eb0331865e813ac589143ce777
moonblade <moonblade168@gmail.com>
2017-04-20 18:57:13 +0530

    eliminate defects excluding one line

--- hand.cpp
+++ hand.cpp
@@ -35,6 +35,44 @@ void Hand::initVectors()
 	defects=vector<vector<Vec4i> > (contours.size());
 }
 
+float Hand::distanceP2P(Point a, Point b){
+	float d= sqrt(fabs( pow(a.x-b.x,2) + pow(a.y-b.y,2) )) ;  
+	return d;
+}
+
+float Hand::getAngle(Point s, Point f, Point e){
+	float l1 = distanceP2P(f,s);
+	float l2 = distanceP2P(f,e);
+	float dot=(s.x-f.x)*(e.x-f.x) + (s.y-f.y)*(e.y-f.y);
+	float angle = acos(dot/(l1*l2));
+	angle=angle*180/PI;
+	return angle;
+}
+
+void Hand::eliminateDefects()
+{
+	int tolerance =  boundingRectangle.height/5;
+	float angleTol=95;
+	vector<Vec4i> newDefects;
+	int startidx, endidx, faridx;
+	vector<Vec4i>::iterator d=defects[bigIndex].begin();
+	for(auto iterator=defects[bigIndex].begin(); d!=defects[bigIndex].end(); d++ ) {
+   	    Vec4i& v=(*d);
+	    startidx=v[0]; Point ptStart(contours[bigIndex][startidx] );
+   		endidx=v[1]; Point ptEnd(contours[bigIndex][endidx] );
+  	    faridx=v[2]; Point ptFar(contours[bigIndex][faridx] );
+		if(distanceP2P(ptStart, ptFar) > tolerance && distanceP2P(ptEnd, ptFar) > tolerance && getAngle(ptStart, ptFar, ptEnd  ) < angleTol ){
+			if( ptEnd.y > (boundingRectangle.y + boundingRectangle.height -boundingRectangle.height/4 ) ){
+			}else if( ptStart.y > (boundingRectangle.y + boundingRectangle.height -boundingRectangle.height/4 ) ){
+			}else {
+				newDefects.push_back(v);		
+			}
+		}	
+	}
+	defects[bigIndex].swap(newDefects);
+	// removeRedundantEndPoints(defects[bigIndex], m);
+}
+
 void Hand::makeContours(Frame frame)
 {
 	f = frame;	
@@ -49,7 +87,7 @@ void Hand::makeContours(Frame frame)
 		approxPolyDP( Mat(hullP[bigIndex]), hullP[bigIndex], 18, true );
 		if(handContour.size()>3 ){
 			convexityDefects(handContour,hullI[bigIndex],defects[bigIndex]);
-			// eleminateDefects(m);
+			eliminateDefects();
 			drawContours(f.src, hullP, bigIndex, Scalar(200,0,0), 2, 8, vector<Vec4i>(), 0, Point());
 		}		
 	}
