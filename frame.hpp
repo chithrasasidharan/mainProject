56c4784b01209414b1e9e6b25f4e732bc55ca1f9
moonblade <moonblade168@gmail.com>
2017-04-21 11:15:22 +0530

    < "

--- frame.hpp
+++ frame.hpp
@@ -1,8 +1,8 @@
 #ifndef _FRAME_
 #define _FRAME_
-#include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/imgproc/types_c.h"
-#include "opencv2/highgui/highgui_c.h"
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/imgproc/types_c.h>
+#include <opencv2/highgui/highgui_c.h>
 #include <opencv2/opencv.hpp>
 using namespace std;
 using namespace cv;
